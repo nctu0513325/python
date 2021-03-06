fb1ce820286e02a0ef1f9dca0c1b7b885e5d0dd7
nctu0513325 <sun1827sun@gmail.com>
2021-03-07 03:21:20 +0800

    setting

---
+++ practice.py
@@ -0,0 +1,5 @@
+x = 10
+y = 2
+z = x / y
+w = x * y
+print(x, y, z, w, sep = " >> ", end = " [OK]" )
---
+++ textbook/example/ch5/ch5_4.py
@@ -0,0 +1,17 @@
+# ch5_4.py
+print("奇數偶數判斷")
+num = input("請輸入任意整值: ")
+rem = int(num) % 2
+if (rem == 0):
+    print("%d 是偶數" % int(num))
+else:
+    print("%d 是奇數" % int(num))
+# PEP 8
+if rem:
+    print("%d 是奇數" % int(num))
+else:
+    print("%d 是偶數" % int(num))
+# 高手用法
+print("%d 是奇數" % int(num) if rem else "%d 是偶數" % int(num))
+
+
