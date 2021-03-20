4dfb75723215ad76777f5fcec2fdbc852198041b
nctu05133325 <sun1827sun@gmail.com>
2021-03-20 18:18:49 +0800

    GA

--- Algorithm/0322/ex-1.py
+++ Algorithm/0322/ex-1.py
@@ -3,10 +3,10 @@ import math
 
 # ==== 參數設定(與演算法相關) ====
 
-NUM_ITERATION = 100			# 世代數(迴圈數)
+NUM_ITERATION = 4000		# 世代數(迴圈數)
 
 NUM_CHROME = 20             # 染色體個數
-NUM_BIT = 30                 # 染色體長度
+NUM_BIT = 30                # 染色體長度
 SIGMA = 0.2                 # 生成子代時用到的干擾
 
 np.random.seed(0)          # 若要每次跑得都不一樣的結果，就把這行註解掉
@@ -14,14 +14,13 @@ np.random.seed(0)          # 若要每次跑得都不一樣的結果，就把這
 # ==== 基因演算法會用到的函式 ====
 def initPop():             # 初始化群體
     # 產生 NUM_CHROME * NUM_BIT 個[-500, 500]之間的隨機數
-	return np.random.uniform(low=-500, high=500, size=(NUM_CHROME,NUM_BIT)) 
+	return np.random.uniform(low=-500, high=500, size=( NUM_CHROME, NUM_BIT )) 
 
 def fitFunc(x):            # 適應度函數
     n = 0
-    #return ((-x[0]) * math.sin(math.sqrt(abs(x[0]))))
-    for i in range(30):
-        n += -((x[0]) * math.sin(math.sqrt(abs(x[0]))))
-    return n
+    for i in range(len(x)):
+        n += (x[0]) * math.sin(math.sqrt(abs(x[0])))        
+    return n   
 
 def evaluatePop(p):        # 評估群體之適應度
     return [fitFunc(p[i]) for i in range(len(p))]
@@ -42,7 +41,7 @@ def reproduction(p):       # 繁衍子代
 #    return kid
 
 def replace(p, p_fit, k, k_fit):            # 適者生存
-    worstIndex = np.argmin(p_fit)
+    worstIndex = np.argmax(p_fit)
     p[worstIndex] = k
     p_fit[worstIndex] = k_fit
     
@@ -52,13 +51,14 @@ def replace(p, p_fit, k, k_fit):            # 適者生存
 pop = initPop()             # 初始化 pop
 pop_fit = evaluatePop(pop)  # 算 pop 的 fit
 
+
 for i in range(NUM_ITERATION) :
     parent = selection(pop)                     # 挑父母
     kid = reproduction(parent)                  # 生子
     kid_fit = fitFunc(kid)                      # 算子代的 fit
     pop, pop_fit = replace(pop, pop_fit, kid, kid_fit)    # 取代
-
-    bestIndex = np.argmax(pop_fit)              # 找此世代最佳解的索引值
-    print('iteration %d:  y = %f'	%(i, pop_fit[bestIndex]))
+    
+    bestIndex = np.argmin(pop_fit)              # 找此世代最佳解的索引值
+print('iteration %d:  y = %f'	%(i + 1, pop_fit[bestIndex]))
 
     
--- Algorithm/0322/ex-3.py
+++ Algorithm/0322/ex-3.py
@@ -3,22 +3,28 @@ import math
 
 # ==== 參數設定(與演算法相關) ====
 
-NUM_ITERATION = 4000			# 世代數(迴圈數)
+NUM_ITERATION = 4000		# 世代數(迴圈數)
 
 NUM_CHROME = 20             # 染色體個數
-NUM_BIT = 2                 # 染色體長度
+NUM_BIT = 1                # 染色體長度
 SIGMA = 0.2                 # 生成子代時用到的干擾
 
 np.random.seed(0)          # 若要每次跑得都不一樣的結果，就把這行註解掉
 
 # ==== 基因演算法會用到的函式 ====
 def initPop():             # 初始化群體
-    # 產生 NUM_CHROME * NUM_BIT 個[-100, 100]之間的隨機數
-	return np.random.uniform(low=-100, high=100, size=(NUM_CHROME,NUM_BIT)) 
+    # 產生 NUM_CHROME * NUM_BIT 個[-500, 500]之間的隨機數
+	return np.random.uniform(low=-32, high=32, size=( NUM_CHROME, NUM_BIT )) 
 
-def fitFunc(x):            # 適應度函數
-    return 0.5 + ((math.sin(math.hypot(x[0], x[1])))**2 - 0.5) \
-           / (1.0 + 0.001 * ( x[0]**2 + x[1]**2 ) )**2
+def fitFunc(x):            # 適應度函數 x[i] ** 2 - 10 * math.cos(2 * math.pi * x[i]) + 10
+    n = 0
+    m = 0
+    y = 0
+    for i in range(NUM_BIT):
+        n += x[0] ** 2 
+        m += math.cos(2 * math.pi * x[0])
+    y = -20 * math.exp(-0.2 * math.sqrt( n / NUM_BIT)) - math.exp(m / NUM_BIT) + 20 +math.exp(1)
+    return y   
 
 def evaluatePop(p):        # 評估群體之適應度
     return [fitFunc(p[i]) for i in range(len(p))]
@@ -39,7 +45,7 @@ def reproduction(p):       # 繁衍子代
 #    return kid
 
 def replace(p, p_fit, k, k_fit):            # 適者生存
-    worstIndex = np.argmin(p_fit)
+    worstIndex = np.argmax(p_fit)
     p[worstIndex] = k
     p_fit[worstIndex] = k_fit
     
@@ -47,12 +53,10 @@ def replace(p, p_fit, k, k_fit):            # 適者生存
 
 # ==== 主程式 ====
 pop = initPop()             # 初始化 pop
-print (pop)
-print("change")
-print(pop[1][0])
-print(pop[0][0])
+print(len(pop))
 
 pop_fit = evaluatePop(pop)  # 算 pop 的 fit
+
 
 for i in range(NUM_ITERATION) :
     parent = selection(pop)                     # 挑父母
@@ -60,7 +64,7 @@ for i in range(NUM_ITERATION) :
     kid_fit = fitFunc(kid)                      # 算子代的 fit
     pop, pop_fit = replace(pop, pop_fit, kid, kid_fit)    # 取代
 
-    bestIndex = np.argmax(pop_fit)              # 找此世代最佳解的索引值
-print('iteration %d: x = %s, y = %f'	%(i, pop[bestIndex], pop_fit[bestIndex]))
+    bestIndex = np.argmin(pop_fit)              # 找此世代最佳解的索引值
+print('iteration %d:  y = %f'	%(i + 1, -pop_fit[bestIndex]))
 
     
