68decba8da6d8e7139d65c217d25011bd53f8daa
nctu0513325 <sun1827sun@gmail.com>
2021-05-14 03:36:32 +0800

    bug 解決

--- Algorithm/final_project/Dataread.py
+++ Algorithm/final_project/Dataread.py
@@ -1,17 +1,12 @@
 def list_order(y):
     pos_1 = [i for i, x in enumerate(y) if x == -1]      #負數位置
     pos_2 = [i for i, x in enumerate(y) if (x > 0 or x ==0)]
-    print(max(pos_2))
-    print(min(pos_1))
     
     while ((max(pos_2)+1) != (min(pos_1))):
         
         y[min(pos_1)], y[max(pos_2)] = y[max(pos_2)], y[min(pos_1)]
         pos_1 = [i for i, x in enumerate(y) if x == -1]      #負數位置
-        pos_2 = [i for i, x in enumerate(y) if x > 0]
-        print("pos_1 = ", pos_1)
-        print("pos_2 = ", pos_2)
-        print(y, len(y))
+        pos_2 = [i for i, x in enumerate(y) if x >= 0]
         
         
     '''for i in range(len(y)-1):
